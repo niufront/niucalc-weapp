8c3f36a93de9b431c9f4d027f5da0b14ad4c7c38
unknown <unknown@example.com>
2025-02-07 16:57:37 +0800

    Initial Commit

---
+++ pages/my/my.js
@@ -0,0 +1,14 @@
+Page({
+  ml1() {
+    console.log('ml1点击')
+  },
+  ml2() {
+    console.log('ml2点击')
+  },
+  ml3() {
+    console.log('ml3点击')
+  },
+  ml4(event) {
+    console.log(event)
+  },
+})
